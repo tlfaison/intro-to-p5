d477e7a82c7d9993348b50242c265f9b0172b962
tlfaison <tlfaison@wlapcs.org>
2017-03-06 20:43:36 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+  function setup(){
+    createCanvas(600,400);
+    background(66,244,170);
+  
+}
+
+function draw(){
+    fill(50);
+    rect(290,100,250,250);
+    
+    fill(125);
+    ellipse(50,250,80,80);
+    
+    line(35,80,400,200);
+    stroke(255,255,255);
+    strokeWeight(4);
+    
+}
